246b4b9a73d8fb59e2a2b73e2e6cbd6cd8a2efc5
Natalia Matat <n.matat@gmail.com>
2022-08-22 15:33:33 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>test message</h1>
+				<img src="./book.png" alt="logo" />
+			</header>
+		</div>
+	);
+}
+
+export default App;
