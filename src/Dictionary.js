828911da1b8d15ee7e55b9b44c05eb2aa958a516
Natalia Matat <n.matat@gmail.com>
2022-08-25 17:07:36 +0300

    add api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './Dictionary.css';
 
 export default function Dictionary() {
 	let [keyword, setKeyword] = useState('');
 
-	function search(event) {
-		event.preventDefault();
-		alert(`Searching ${keyword}`);
+	function handleResponse(response) {
+		console.log(response.data[0]);
 	}
 
+	function search(event) {
+		event.preventDefault();
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		console.log(apiUrl);
+		axios.get(apiUrl).then(handleResponse);
+	}
+
+	// https://api.dictionaryapi.dev/api/v2/entries/en/hello
 	function handleKeywordChange(event) {
 		setKeyword(event.target.value);
 		console.log(keyword);
