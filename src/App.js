1e288d2bb6da081c9089cf32e4ccf6262a425c99
Natalia Matat <n.matat@gmail.com>
2022-08-25 16:23:11 +0300

    add search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import Dictionary from './Dictionary';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1>test message</h1>
-				<img src="./book.png" alt="logo" />
-			</header>
+			<div className="container">
+				<header className="App-header">
+					<h1>
+						<img src="./book.png" alt="logo" />
+						Welcome to Dictionary
+					</h1>
+				</header>
+				<main className="Dictionary">
+					<Dictionary />
+				</main>
+				<footer className="text-center App-footer">Coded by</footer>
+			</div>
 		</div>
 	);
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './Dictionary.css';
+
+export default function Dictionary() {
+	let [keyword, setKeyword] = useState('');
+
+	function search(event) {
+		event.preventDefault();
+		alert(`Searching ${keyword}`);
+	}
+
+	function handleKeywordChange(event) {
+		setKeyword(event.target.value);
+		console.log(keyword);
+	}
+
+	return (
+		<div className="Dictionary">
+			<form onSubmit={search}>
+				<input type="search" autoFocus={true} onChange={handleKeywordChange} />
+			</form>
+		</div>
+	);
+}
